682639099e584673feabc32221f904cd7875e67b
Krisztián Ádám <adamkrisz@gmail.com>
2022-02-06 18:10:28 +0100

    Update 2022

--- _/build.rb
+++ _/build.rb
@@ -4,5 +4,6 @@ Dir.glob('pages/*.html.erb').each do |page_path|
   content = eval(Erubi::Engine.new(File.read(page_path)).src)
   content = eval(Erubi::Engine.new(File.read('page.html.erb')).src)
 
+  puts page_path
   File.write('../' + page_path.split('/')[1].split('.')[0] + '.html', content)
 end
